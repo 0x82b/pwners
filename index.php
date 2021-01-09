17c4ddfe6bebfccad2d7052c97e8cc743185e7ce
0x82b <75819560+0x82b@users.noreply.github.com>
2021-01-09 15:36:23 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+    <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>pwned</title>
+    </head>
+<style>
+	body {
+    background: #202020;
+    color: white;
+    font-family: monospace;
+    }
+    </style>
+    <center>
+<body>
+        <pre>
+                                                              
+                                                              
+                                                              
+▀████████▄▀██▀    ▄█    ▀██▀████████▄   ▄▄█▀██▀███▄███ ▄██▀███
+  ██   ▀██  ██   ▄███   ▄█   ██    ██  ▄█▀   ██ ██▀ ▀▀ ██   ▀▀
+  ██    ██   ██ ▄█  ██ ▄█    ██    ██  ██▀▀▀▀▀▀ ██     ▀█████▄
+  ██   ▄██    ███    ███     ██    ██  ██▄    ▄ ██     █▄   ██
+  ██████▀      █      █    ▄████  ████▄ ▀█████▀████▄   ██████▀
+  ██                                                          
+▄████▄                                                        
+
+        </pre>
+        <p>Hello We are pwners!</p>
+        <p>We are a Grey hat hacking group. We only hack to troll, make money and for fun.</p>
+        <br>
+        <p>Donations are appreciated :)</p>
+        
+    </body>
+    </center>
+</html>
