b3bf81dc83b7238ce50150bbd8f22076f497472b
0x82b <75819560+0x82b@users.noreply.github.com>
2021-01-09 15:38:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,23 +14,69 @@
     <center>
 <body>
         <pre>
-                                                              
-                                                              
-                                                              
-▀████████▄▀██▀    ▄█    ▀██▀████████▄   ▄▄█▀██▀███▄███ ▄██▀███
-  ██   ▀██  ██   ▄███   ▄█   ██    ██  ▄█▀   ██ ██▀ ▀▀ ██   ▀▀
-  ██    ██   ██ ▄█  ██ ▄█    ██    ██  ██▀▀▀▀▀▀ ██     ▀█████▄
-  ██   ▄██    ███    ███     ██    ██  ██▄    ▄ ██     █▄   ██
-  ██████▀      █      █    ▄████  ████▄ ▀█████▀████▄   ██████▀
-  ██                                                          
-▄████▄                                                        
+ ██▓███   █     █░ ███▄    █  ▓█████ ██▀███    ██████ 
+▓██░  ██ ▓█░ █ ░█░ ██ ▀█   █  ▓█   ▀▓██ ▒ ██▒▒██    ▒ 
+▓██░ ██▓▒▒█░ █ ░█ ▓██  ▀█ ██▒ ▒███  ▓██ ░▄█ ▒░ ▓██▄   
+▒██▄█▓▒ ▒░█░ █ ░█ ▓██▒  ▐▌██▒ ▒▓█  ▄▒██▀▀█▄    ▒   ██▒
+▒██▒ ░  ░░░██▒██▓ ▒██░   ▓██░▒░▒████░██▓ ▒██▒▒██████▒▒
+▒▓▒░ ░  ░░ ▓░▒ ▒  ░ ▒░   ▒ ▒ ░░░ ▒░ ░ ▒▓ ░▒▓░▒ ▒▓▒ ▒ ░
+░▒ ░       ▒ ░ ░  ░ ░░   ░ ▒░░ ░ ░    ░▒ ░ ▒ ░ ░▒  ░ ░
+░░         ░   ░     ░   ░ ░     ░    ░░   ░ ░  ░  ░  
+             ░             ░ ░   ░     ░           ░  
+                                                     
 
         </pre>
         <p>Hello We are pwners!</p>
         <p>We are a Grey hat hacking group. We only hack to troll, make money and for fun.</p>
         <br>
         <p>Donations are appreciated :)</p>
-        
+        <div id="cd45"></div>
+<script>
+  !function(c){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.onload=c,t.src="//lab.subinsb.com/projects/francium/cryptodonate/widget.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}(function(){
+    Fr.loadCD("cd45", {
+      coin: "bitcoin",
+      address: "17x9zC5voBnnYcmbNCKJYsePuF8Rwh39Lp",
+      buttonClass: "dark",
+      dialogClass: "dark",
+    });
+  });
+</script>
+<br />
+<div id="cd706"></div>
+<script>
+  !function(c){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.onload=c,t.src="//lab.subinsb.com/projects/francium/cryptodonate/widget.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}(function(){
+    Fr.loadCD("cd706", {
+      coin: "litecoin",
+      address: "LVmdVdfYVeuqt36Nuo5kUwZKmA25Wxj8q8",
+      buttonClass: "dark",
+      dialogClass: "dark",
+    });
+  });
+</script>
+<br />
+<div id="cd28"></div>
+<script>
+  !function(c){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.onload=c,t.src="//lab.subinsb.com/projects/francium/cryptodonate/widget.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}(function(){
+    Fr.loadCD("cd28", {
+      coin: "ethereum",
+      address: "0x568C6C3AEc245a08EB6c17AABF8265dC87669B60",
+      buttonClass: "dark",
+      dialogClass: "dark",
+    });
+  });
+</script>
+<br />
+<div id="cd783"></div>
+<script>
+  !function(c){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.onload=c,t.src="//lab.subinsb.com/projects/francium/cryptodonate/widget.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}(function(){
+    Fr.loadCD("cd783", {
+      coin: "monero",
+      address: "47WW2N5W1NiHiokRC6rFngTTyTcGNbdnKcHnEVB5XoHDDisgE1cDDWRgqSQSmdj7VfdMErRdXfZVFSbQhawimYzd2p4sGPk",
+      buttonClass: "dark",
+      dialogClass: "dark",
+    });
+  });
+</script>
     </body>
     </center>
 </html>
